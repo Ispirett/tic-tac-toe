f6866e7cff9c5c65719871b2fbf1388e27344bfb
Armando Jose Soto Melo <armando29@live.com>
2019-05-10 14:28:50 -0400

    Update game_engine.rb

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -1,18 +1,17 @@
 module GameEngine
+  public
 
-public
+  FILE_NAME = "score.txt".freeze
 
-FILE_NAME = 'score.txt'.freeze
-
-def open_file
-  puts File.open(FILE_NAME, 'r', &:read)
-end
-
-def create_or_append_file(data)
-  File.open(FILE_NAME, 'a+') do |file|
-    file.puts(data)
+  def open_file
+    puts File.open(FILE_NAME, "r", &:read)
   end
-end
+
+  def create_or_append_file(data)
+    File.open(FILE_NAME, "a+") do |file|
+      file.puts(data)
+    end
+  end
 end
 
 class String
@@ -48,4 +47,55 @@ class String
     colorize(15)
   end
 
+  def bg_black
+    colorize(40)
+  end
+
+  def bg_red
+    colorize(41)
+  end
+
+  def bg_green
+    colorize(42)
+  end
+
+  def bg_brown
+    colorize(43)
+  end
+
+  def bg_blue
+    colorize(44)
+  end
+
+  def bg_magenta
+    colorize(45)
+  end
+
+  def bg_cyan
+    colorize(46)
+  end
+
+  def bg_gray
+    colorize(47)
+  end
+
+  def bold
+    colorize(22)
+  end
+
+  def italic
+    colorize(23)
+  end
+
+  def underline
+    colorize(24)
+  end
+
+  def blink
+    colorize(25)
+  end
+
+  def reverse_color
+    colorize(27)
+  end
 end
