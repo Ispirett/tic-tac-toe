4d4d5b2705e46597351f4148017fb75aa3cc580b
Armando Jose Soto Melo <armando29@live.com>
2019-05-10 14:29:32 -0400

    Update gui.rb

--- lib/gui.rb
+++ lib/gui.rb
@@ -1,17 +1,17 @@
 class Gui
+  def initialize()
+  end
 
   def display_board(slots)
     row_one = "\t\t\t\t\t\t#{slots[:one]}|#{slots[:two]}|#{slots[:three]}"
     row_two = "\t\t\t\t\t\t#{slots[:four]}|#{slots[:five]}|#{slots[:six]}"
     row_three = "\t\t\t\t\t\t#{slots[:seven]}|#{slots[:eight]}|#{slots[:nine]}"
-    [row_one, row_two, row_three]
+    space = "\n"
+    [row_one, row_two, row_three, space]
   end
 
-  def msg
+  def msg(msg)
     return msg.each { |m| puts m } if msg.is_a? Array
     puts "\t\t\t\t\t#{msg}"
   end
-
-
 end
-
