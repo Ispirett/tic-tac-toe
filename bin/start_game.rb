a34e18ffff35ae204f5203ffc719bac87e400cae
Macatuz <armando29@live.com>
2019-05-10 13:57:39 -0400

    approach more colors plus space game_start

---
+++ bin/start_game.rb
@@ -0,0 +1,9 @@
+require "../lib/game_manager"
+require "../lib/board"
+require "../lib/gui"
+
+display = Gui.new
+app = GameManager.new(display)
+player = app.game_start
+board = Board.new(player[0], player[1], display, app)
+board.game_loop
