951286baebfa7fc716ced688a8e2501525311020
Armando Jose Soto Melo <armando29@live.com>
2019-05-10 14:29:49 -0400

    Update player.rb

---
+++ lib/player.rb
@@ -0,0 +1,20 @@
+class Player
+  def initialize(name)
+    @name = name
+    @icon = "NNN"
+  end
+
+  def name
+    @name
+  end
+
+  def icon
+    @icon
+  end
+
+  def icon=(string)
+    @icon = string
+  end
+end
+
+# icon
